f7b1fcdc53118b717d564e33ccaeb5fd0f5bb4f8
Alejandro Bernal <alejandrobernalduque8@gmail.com>
2023-06-21 18:46:45 -0500

    Subir Lading page
    He subido todos mis archivos

---
+++ JS/menu.js
@@ -0,0 +1,15 @@
+(function () {
+
+  const openButton = document.querySelector('.nav__menu');
+  const menu = document.querySelector('.nav__link');
+  const closeMenu = document.querySelector('.nav__close');
+
+  openButton.addEventListener('click', function () {
+    menu.classList.add('nav__link--show');
+  });
+
+  closeMenu.addEventListener('click', function () {
+    menu.classList.remove('nav__link--show');
+  });
+
+})();
